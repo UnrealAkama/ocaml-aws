9d964ef2e0a1ca8b6cacef9bc4f648b6a5434990
Adam <adam@nexadmin.com>
2020-07-01 22:03:17 -0500

    footguns are bad.

--- libraries/kms/lib/sign.ml
+++ libraries/kms/lib/sign.ml
@@ -16,8 +16,8 @@ let to_http service region req =
 let of_http body =
   try
     let xml = Ezxmlm.from_string body in
-    let resp = Ezxmlm.member "SignResponse" (snd xml) in
-    let resp = Xml.member "SignResult" resp in
+    let resp = Xml.member "SignResponse" (snd xml) in
+    let resp = Util.option_bind resp (Xml.member "SignResult") in
     try
       Util.or_error (Util.option_bind resp SignResponse.parse)
         (let open Error in
